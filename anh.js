a85fb6a1ff0ec09b86f516bb3854ced0f133b8e8
TranMaiHuong300698 <37067046+TranMaiHuong300698@users.noreply.github.com>
2018-04-11 14:56:53 +0700

    Update anh.js

--- anh.js
+++ anh.js
@@ -7,7 +7,27 @@ $(document).ready(function() {
        $('#navfix').removeClass('color');
   });
 });
+  var i = 0;
+  var images = [];
+  var time =2000;
+  images[0] = 'anh2.jpg';
+  images[1] = 'anh3.jpg';
+  images[2] = 'anh4.jpg';
+  images[3] = 'anh5.jpg';
+  images[4] = 'anh8.jpg';
+  starImg = $("img.slide:first").attr("i");
+  endImg = $("img.slide:last").attr("i");
+function changeImage() {
+  document.slide.src = images[i];
+  if (i<images.length-1)
+    i++;
+  else
+    i=0;
+  setTimeout("changeImage()",time);
+}
+window.onload = changeImage;
 
+/* nội dung */
 $(function () {
 
   var filterList = {
